913f54649efae5b1cae512f9cb6d060e6efe8e00
Travis Reed <tdreed@gmail.com>
2025-04-10 10:53:52 -0700

    Remove debug artifacts

--- thing_macros/target/debug/deps/thing_macros-0dddea2cb5817789.d
+++
@@ -1,5 +0,0 @@
-/home/dancxjo/src/basic-os/thing_macros/target/debug/deps/libthing_macros-0dddea2cb5817789.rmeta: src/lib.rs
-
-/home/dancxjo/src/basic-os/thing_macros/target/debug/deps/thing_macros-0dddea2cb5817789.d: src/lib.rs
-
-src/lib.rs:
--- thing_macros/target/debug/deps/thing_macros-c9662a31432845b6.d
+++
@@ -1,5 +0,0 @@
-/home/dancxjo/src/basic-os/thing_macros/target/debug/deps/libthing_macros-c9662a31432845b6.rmeta: src/lib.rs
-
-/home/dancxjo/src/basic-os/thing_macros/target/debug/deps/thing_macros-c9662a31432845b6.d: src/lib.rs
-
-src/lib.rs:
