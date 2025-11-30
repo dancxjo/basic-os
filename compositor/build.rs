24aa70515208ad67efd60dbbc07480c40300ee34
Travis Reed <tdreed@gmail.com>
2025-11-29 21:27:05 -0800

    Update Cargo.toml to add tiny_http as an optional dependency; modify build.rs to conditionally link against the linker script for non-linux targets

--- compositor/build.rs
+++ compositor/build.rs
@@ -11,9 +11,12 @@ const UNIFONT_GZ: &str = "unifont-15.1.05.hex.gz";
 const UNIFONT_HEX: &str = "unifont-15.1.05.hex";
 
 fn main() {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-    println!("cargo:rustc-link-arg=-T{}/link.ld", manifest_dir);
-    println!("cargo:rerun-if-changed=link.ld");
+    let target = env::var("TARGET").expect("TARGET not set");
+    if !target.contains("linux") {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+        println!("cargo:rustc-link-arg=-T{}/link.ld", manifest_dir);
+        println!("cargo:rerun-if-changed=link.ld");
+    }
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
     let gz_path = out_dir.join(UNIFONT_GZ);
