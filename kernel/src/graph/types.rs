ad0142fbcec06c6b3e72fa6e5775710947a8abcc
Travis Reed <tdreed@gmail.com>
2025-11-30 12:42:31 -0800

    fix: reorder imports for consistency in graph types module

--- kernel/src/graph/types.rs
+++ kernel/src/graph/types.rs
@@ -3,11 +3,11 @@ pub use thing_abi::{
     GrantCapabilityRequest, GraphChange, GraphEdge, GraphFiatRequest, GraphFindByKind,
     GraphFindResultHeader, GraphGetRequest, GraphLinkRequest, GraphNodeRequest,
     GraphPropsGetRequest, GraphPropsRequest, GraphSnapshot, GraphThatRequest, GraphThing,
-    GraphWatchBatch, NodePattern, QueueStateSpec, SharedBufferSpec, Symbol,
-    Value, WatchId, canon, cc, from_char, from_u16,
+    GraphWatchBatch, NodePattern, QueueStateSpec, SharedBufferSpec, Symbol, Value, WatchId, canon,
+    cc, from_char, from_u16,
 };
 
-pub use thingos_kernel_std::id::{BundleId, ThingId, PredId, KERNEL_BUNDLE_ID};
+pub use thingos_kernel_std::id::{BundleId, KERNEL_BUNDLE_ID, PredId, ThingId};
 
 pub use thing_abi::{
     canon as canon_sym, cc as cc_sym, from_char as symbol_from_char, from_u16 as symbol_from_u16,
